0a9c51796c0ee30ac1bc7484a97898aa85802bb3
bouchakour alaa <85706955+bouchakour-02@users.noreply.github.com>
2024-08-02 13:01:49 +0100

    Initial commit - setup backend with authentication

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    console.log('MONGO_URI:', process.env.MONGO_URI); // Log the URI to ensure it's being read correctly
+    console.log('Connecting to MongoDB...');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
