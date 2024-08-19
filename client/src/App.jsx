35c3d52dc95d82ca876f48be11c5a38ac1105f22
bouchakour alaa <85706955+bouchakour-02@users.noreply.github.com>
2024-08-19 13:14:57 +0100

    frontend works correctely  commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Profile from './components/Profile.jsx';
-import CreateProfile from './components/CreateProfile.jsx';
-import Login from './components/Login.jsx';
-import Plant from './components/Plant.jsx';
-import GardenEvent from './components/GardenEvent.jsx';
-import CommunityPost from './components/CommunityPost.jsx';
-import ProtectedRoute from './components/ProtectedRoute.jsx';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Profile from "./components/Profile";
+import CreateProfile from "./components/CreateProfile";
+import Login from "./components/Login";
+import Plant from "./components/Plant";
+import GardenEvent from "./components/GardenEvent";
+import CommunityPost from "./components/CommunityPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Switch>
-                    <Route path="/login" component={Login} />
-                    <ProtectedRoute path="/profile" component={Profile} />
-                    <ProtectedRoute path="/create-profile" component={CreateProfile} />
-                    <ProtectedRoute path="/plant" component={Plant} />
-                    <ProtectedRoute path="/garden-event" component={GardenEvent} />
-                    <ProtectedRoute path="/community-post" component={CommunityPost} />
-                    <Route path="/" exact component={() => <div>Home Page</div>} />
-                </Switch>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/profile"
+            element={<ProtectedRoute element={<Profile />} />}
+          />
+          <Route
+            path="/create-profile"
+            element={<ProtectedRoute element={<CreateProfile />} />}
+          />
+          <Route
+            path="/plants"
+            element={<ProtectedRoute element={<Plant />} />}
+          />
+          <Route
+            path="/garden-event"
+            element={<ProtectedRoute element={<GardenEvent />} />}
+          />
+          <Route
+            path="/community-post"
+            element={<ProtectedRoute element={<CommunityPost />} />}
+          />
+          <Route path="/" element={<div>Home Page</div>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,21 +1,15 @@
-// src/components/ProtectedRoute.js
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { Route, Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ component: Component, ...rest }) => {
-    const isAuthenticated = !!localStorage.getItem('token');
-    return (
-        <Route
-            {...rest}
-            render={props =>
-                isAuthenticated ? (
-                    <Component {...props} />
-                ) : (
-                    <Redirect to="/login" />
-                )
-            }
-        />
-    );
+const ProtectedRoute = ({ element: Component, ...rest }) => {
+  const isAuthenticated = !!localStorage.getItem("token");
+  return (
+    <Route
+      {...rest}
+      element={isAuthenticated ? <Component /> : <Navigate to="/login" />}
+    />
+  );
 };
 
 export default ProtectedRoute;
+//redirecte changed with navigate
